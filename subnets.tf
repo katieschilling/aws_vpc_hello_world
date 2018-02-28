1ac98966abeadab43c0c5276048f40f40cb4b10e
Katie Schilling <kdschilling@gmail.com>
2018-02-28 15:41:38 -0600

    Add subnets

--- subnets.tf
+++ subnets.tf
@@ -1,2 +1,19 @@
-resource "aws_subnet" "public" {}
-resource "aws_subnet" "private" {}
+resource "aws_subnet" "public" {
+  cidr_block = "${var.public_subnet_cidr_block}"
+  vpc_id     = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name     = "${var.tag_name}"
+    type     = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  cidr_block = "${var.private_subnet_cidr_block}"
+  vpc_id     = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name     = "${var.tag_name}"
+    type     = "private"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,13 @@ variable "vpc_cidr_block" {
   description = "CIDR for the VPC"
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidr_block" {
+  description = "CIDR for the public subnet"
+  default     = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "CIDR for the private subnet"
+  default     = "10.0.1.0/24"
+}
