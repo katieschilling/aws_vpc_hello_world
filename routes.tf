58edc2a59d30f5a5e904a1d127106c8692db4d6e
Katie Schilling <kdschilling@gmail.com>
2018-02-28 16:27:04 -0600

    Add route tables & associations

--- routes.tf
+++ routes.tf
@@ -1,4 +1,37 @@
-resource "aws_route_table" "public" {}
-resource "aws_route_table" "private" {}
-resource "aws_route_table_association" "public" {}
-resource "aws_route_table_association" "private" {}
+resource "aws_route_table" "public" {
+  vpc_id           = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = "${aws_internet_gateway.ig.id}"
+  }
+
+  tags {
+    Name           = "${var.tag_name}_public_routes"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id           = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat_gw.id}"
+  }
+
+  tags {
+    Name           = "${var.tag_name}_public_routes"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id        = "${aws_subnet.public.id}"
+  route_table_id   = "${aws_route_table.public.id}"
+
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id        = "${aws_subnet.private.id}"
+  route_table_id   = "${aws_route_table.private.id}"
+}
+
